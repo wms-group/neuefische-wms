a86046ca9ccf272d7d24e3ab9ffb1c69ba1fabd3
gerald <geraldmontufar@gmail.com>
2025-05-13 09:33:38 +0200

    initial commit

---
+++ backend/src/main/java/com/wmsgroup/neuefische_wms/NeuefischeWmsApplication.java
@@ -0,0 +1,13 @@
+package com.wmsgroup.neuefische_wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuefischeWmsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wmsgroup/neuefische_wms/NeuefischeWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmsgroup.neuefische_wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
