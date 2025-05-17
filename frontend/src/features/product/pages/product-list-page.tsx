dbac8c69d7f40bf25004f3c56d48d0a9f63bb50a
Carsten Kollmeier <carsten@ckollmeier.de>
2025-05-17 15:04:52 +0200

    Remove unnecessary ESLint disable comment

--- frontend/src/features/product/pages/product-list-page.tsx
+++ frontend/src/features/product/pages/product-list-page.tsx
@@ -25,7 +25,6 @@ const ProductListPage = () => {
             setCategory(undefined);
             setProducts([]);
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [categoryId, categories])
 
     useEffect(() => {
